fab0904b37a23b61ddce0dd7a5f74f8057007319
AN Zhu <zhua@kean.edu>
2018-11-22 12:46:44 -0600

    Black Cube

--- GStar/MainRenderLoop.cpp
+++ GStar/MainRenderLoop.cpp
@@ -1,14 +1,13 @@
 #include"Scene.h"
 #include"View.h"
 #include"Controller.h"
-Scene* myScene;
+Scene* myScene = Scene::Create();
 Controller myController(myScene);
 View myView(myScene);
 void mouse_call(GLFWwindow * window, double xpos, double ypos);
 void framebuffer_size_call(GLFWwindow * windwo, int width, int height);
 void MainEntrance() {
 	//Set up 
-	myScene = Scene::Create();
 	//Bind CallBack Function
 	glfwSetFramebufferSizeCallback(myScene->Window(), framebuffer_size_call);
 	glfwSetCursorPosCallback(myScene->Window(), mouse_call);

117c8b3e68ea2d33f43f93c1b3631807cc811878
AN Zhu <zhua@kean.edu>
2018-12-29 11:58:31 -0600

    Commit

--- GStar/TransformComponent.h
+++ GStar/TransformComponent.h
@@ -4,6 +4,22 @@
 #include "View.h"
 #include "Object.h"
 namespace GStar {
+	//Version 2.0 Check OLDCODE Folder for previous version
+	struct TransformData
+	{
+		Vector3 f;//forward vector
+		Vector3 u;//upward vector
+		Vector3 r;//right vector
+		Vector3 c;//position
+		Vector3 s;//scale
+		inline void Initialize() {
+			f = Vector3(0, 0, 1);
+			u = Vector3(0, 1, 0);
+			r = Vector3(1, 0, 0);
+			c = Vector3(0, 0, 0);
+			s = Vector3(1, 1, 1);
+		}
+	};
 	class TransformComponent:public Component {
 	public:
 		inline void Update() {};
@@ -11,65 +27,49 @@ namespace GStar {
 			my_Object->Update();
 			my_children.Resetcurrent();
 			while (my_children.HasNext()) {
-				my_children.GetNext()->ParentSetModel(getModel());
+				my_children.GetNext()->ParentSetModel(my_model);
 				my_children.GetNext()->WorldUpdate();
 				my_children.Move();
 			} 
 		}
 		inline void AddChildren(TransformComponent* component) {
 			my_children.Push(component);
+			component->SetParent(this);
 		}
-		TransformComponent(Object* object) :Component(TRANSFORM_WORD),model(IDENTICAL_MATRIX),my_Object(object),parent(IDENTICAL_MATRIX) { }
+		TransformComponent(Object* object) :
+			Component(TRANSFORM_WORD),
+			my_parent(nullptr),
+			my_Object(object) { }
+
 		inline void SetTransform(const GStar::Vector3& Tranform) {
-				model.Getreference(0,3) = Tranform.x();
-				model.Getreference(1, 3) = Tranform.y();
-				model.Getreference(2, 3) = Tranform.z();
 		}
 		inline void Translate(const GStar::Vector3& translate) {
-			model.Getreference(0, 3) += translate.x();
-			model.Getreference(1, 3) += translate.y();
-			model.Getreference(2, 3) += translate.z();
 		}
 		inline const GStar::Vector3 GetTransform() const { 
-				return GStar::Vector3(model.GetData(0,3)
-					, model.GetData(1, 3)
-					, model.GetData(2, 3));
 		}
 		inline void SetScale(const GStar::Vector3& Scale) {
-			model.Getreference(0, 0) = Scale.x();
-			model.Getreference(1, 1) = Scale.y();
-			model.Getreference(2, 2) += Scale.z();
 		}
 		inline const GStar::Vector3 getScale() const {
-				return GStar::Vector3(model.GetData(0, 0),
-					model.GetData(1, 1),
-					model.GetData(2, 2));
 		}
 		inline void SetRotation(const GStar::Vector3& Scale) {
-			rotate_X = Scale.x();
-			rotate_Y = Scale.y();
-			rotate_Z = Scale.z();
 		}
 		inline const GStar::Vector3 GetRoatation() {
-			return GStar::Vector3(rotate_X, rotate_Y, rotate_Z);
 		}
-		inline GStar::Matrix4 getModel() {
-			//TODO figure out why. this should be model * parent * vector Probably because OpenGl use Roll matrix
-			return GStar::Rotate(parent.Dot(model), rotate_X, rotate_Y, rotate_Z);
+		inline TransformData getModel() {
+			return my_model;
 		}
 		inline GStar::Matrix4 getModelI() {
-			return GStar::Rotate(parent.Dot(model), rotate_X, rotate_Y, rotate_Z).I();
 		}
-		inline void ParentSetModel( const GStar::Matrix4& input) {
-			parent = input;
+		inline void SetParent(TransformComponent* component) {
+			my_parent = component;
+		}
+		inline void ParentSetModel( const TransformData& data) {
+			my_model = data;
 		}
 	private:
 		SingleLinkedList<GStar::TransformComponent*> my_children;
+		GStar::TransformComponent* my_parent;
 		Object* my_Object;
-		GStar::Matrix4 model;
-		GStar::Matrix4 parent;
-		float rotate_X;
-		float rotate_Y;
-		float rotate_Z;
+		TransformData my_model;
 	};
 }

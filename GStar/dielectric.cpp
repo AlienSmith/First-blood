aba2f999f96d80efc66b894f3b3ed8808cdf5040
AN Zhu <zhua@kean.edu>
2019-02-24 23:11:29 -0600

    Commit

--- GStar/dielectric.cpp
+++ GStar/dielectric.cpp
@@ -4,26 +4,35 @@
 #include "Schlick.h"
 bool GStar::dielectric::scatter(const Ray & r_in, const hit_record & rec, Vector3 & attenuation, Ray & scattered) const
 {
-	GStar::RayData ray = r_in.getValue();
-	GStar::Vector3 outward_normal;
-	GStar::Vector3 reflected = GStar::Vector3::Reflect(ray.direction, rec.n);
+	RayData ray = r_in.getValue();
+	Vector3 outward_normal;
+	Vector3 reflected = Vector3::Reflect(ray.direction, rec.n);
 	float ni_over_nt;
-	attenuation = GStar::Vector3(1.0f, 1.0f, 0.0f);
-	GStar::Vector3 refracted;
+	attenuation = Vector3(1.0f, 1.0f, 1.0f);
+	Vector3 refracted;
+	float reflect_prob;
+	float cosine;
 	if (ray.direction.Dot(rec.n) > 0) {
-		outward_normal = -1.0f*rec.n;
+		outward_normal = -1.0f * rec.n;
 		ni_over_nt = ref_idx;
+		cosine = ref_idx * ray.direction.Dot(rec.n);
 	}
 	else {
 		outward_normal = rec.n;
 		ni_over_nt = 1.0f / ref_idx;
+		cosine = -1.0f * ray.direction.Dot(rec.n);
 	}
-	if (GStar::Vector3::Refract(ray.direction, outward_normal, ni_over_nt, refracted)) {
-		scattered = GStar::Ray(rec.p, refracted);
-		return true;
+	if (Vector3::Refract(ray.direction, outward_normal, ni_over_nt, refracted)) {
+		reflect_prob = schlik(cosine, ref_idx);
 	}
 	else {
-		scattered = GStar::Ray(rec.p, reflected);
-		return false;
+		scattered = Ray(rec.p, reflected);
+		reflect_prob = 1.0f;
+	}if (UnitRandom() <= reflect_prob) {
+		scattered = Ray(rec.p, reflected);
 	}
+	else {
+		scattered = Ray(rec.p, refracted);
+	}
+	return true;
 }

cb811c8a7b5f419217bcee4eaccd37a6086f7dd1
AN Zhu <zhua@kean.edu>
2019-03-04 13:06:54 -0600

    Commit

--- MonsterChase/Sprite.lua
+++ MonsterChase/Sprite.lua
@@ -1,3 +1,9 @@
 Version = "Alpha";
-IsLight = false;
-IsCamera = false;
+Camera ={
+	Name = "Camera",
+	initial_position = {0.0, 0.0, 1.0}
+}
+Sprite = {
+	Name = "Sprite",
+	initial_position = {0.0, 0.0, -0.5}
+}
